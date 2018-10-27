5bc2c70084c55fa3b8ea5e7eefa7e29d90b33d48
Håvard Anda Estensen <haavard.ae@gmail.com>
2018-10-27 12:30:28 +0200

    Use shortid for todo ids

--- src/models.js
+++ src/models.js
@@ -1,6 +1,8 @@
+import shortid from 'shortid'
+
 export default class Todo {
   constructor(description) {
-    this.id = String(Date.now())
+    this.id = shortid.generate()
     this.done = false
     this.description = description
   }
